79a2285f684a6e0ab8e22b6bba83a168d6838c35
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-02-29 16:39:09 -0500

    Stop using the bin folder.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo $TYPE
 
 source ~/.bashrc
 if [ "$TYPE" == "controller" ]; then
-    echo "cd /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > bin/application
+    echo "cd /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > application
 else
     echo "python --version" > application
 fi
