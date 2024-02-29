ce2b3bdd645e84e8f1d4134ef4ae6fe0f4ea6fca
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-02-29 09:45:31 -0500

    Update to write Procfile from build.sh file.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo $TYPE
 source ~/.bashrc
 cd /var/app/current
 if [ "$TYPE" == "controller" ]; then
-    python -m locust -f /var/app/current/websocket_server/locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556
+    echo "web: python -m locust -f /var/app/current/websocket_server/locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > Procfile
 else
-    python --version
+    echo "web: python --version" > Procfile
 fi
