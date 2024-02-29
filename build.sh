d856bc52282db41e6a31f2132bf2be51dc28c195
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-02-29 16:51:06 -0500

    Add pyenv/shims folder to path.

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ echo $TYPE
 
 source ~/.bashrc
 if [ "$TYPE" == "controller" ]; then
-    echo "cd /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > application
+    echo "source /root/.bashrc && cd /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > application
 else
-    echo "python --version" > application
+    echo "source /root/.bashrc && python --version" > application
 fi
 
 chmod 755 application
