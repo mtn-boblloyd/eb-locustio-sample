908ae421cf86b92a1eb749bc8908553863c8426c
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-02-29 15:37:45 -0500

    Minor changes to build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -xe
 echo $TYPE
 
 source ~/.bashrc
-cd /var/app/staging
-mkdir -p bin
 if [ "$TYPE" == "controller" ]; then
-    echo "ced /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > bin/application
+    echo "cd /var/app/current && python -m locust -f locustfile.py --tags load_test --host ws://localhost:10100 -u 100 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --headless --master -P 8088 --master-bind-port 5556" > bin/application
 else
-    echo "python --version" > bin/application
+    echo "python --version" > application
 fi
 
-chmod 755 bin/application
+chmod 755 application
