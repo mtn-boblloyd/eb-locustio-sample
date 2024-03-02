5cd181b57073f5f881f68ef889c080b76c518acd
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-03-02 15:27:30 -0500

    Add logging to debug build.sh failures.

--- build.sh
+++ build.sh
@@ -6,25 +6,35 @@ echo $TYPE
 su webapp <<_
 
 curl https://pyenv.run | bash;
+echo "checking if pyenv is already set up in webapp."
+cat /home/webapp/.bashrc | grep PYENV_ROOT
 if [ "$(cat /home/webapp/.bashrc | grep PYENV_ROOT)" == "" ]; then
+    echo "Inside pyenv bashrc setup"
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /home/webapp/.bashrc
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/webapp/.bashrc
     echo 'export PATH="$PYENV_ROOT/shims:$PATH"' >> /home/webapp/.bashrc
     echo 'eval "$(pyenv init -)"' >> /home/webapp/.bashrc
 fi
 
+echo "checking if python version 3.10.4 is already installed
+ls /home/webapp/.pyenv/versions/3.10.4
 if [ -d /home/webapp/.pyenv/versions/3.10.4/ ]; then
+    echo "Inside python install..."
     source ~/.bashrc
-    eval "$(pyenv init -)"  
+    eval "$(pyenv init -)"
     pyenv install 3.10.4 && pyenv global 3.10.4
     python -m pip install locust==2.16.1 locust-plugins==4.0.0 paho-mqtt==1.6.1 websocket-client==1.6.2 ujson
 fi
 _
+
+echo "Done with webapp user configuration, building the application file."
 
 if [ "$TYPE" == "controller" ]; then
     echo "source ~/.bashrc && cd /var/app/current && python -m locust -f ./http_client/locustfile.py --tags load_test" > application
 else
     echo "source ~/.bashrc && python --version" > application
 fi
+
+cat application
 
 chmod 755 application
