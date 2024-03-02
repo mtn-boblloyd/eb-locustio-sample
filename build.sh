110f83d24b531825c113d97b0dd8f5a89e12268f
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-03-02 14:27:23 -0500

    Fix up the build.sh to error check if things have already been run.

--- build.sh
+++ build.sh
@@ -6,13 +6,15 @@ echo $TYPE
 su webapp <<_
 
 curl https://pyenv.run | bash;
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /home/webapp/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/webapp/.bashrc
-echo 'export PATH="$PYENV_ROOT/shims:$PATH"' >> /home/webapp/.bashrc
-echo 'eval "$(pyenv init -)"' >> /home/webapp/.bashrc
+if [ "$(cat /home/webapp/.bashrc | grep PYENV_ROOT)" == "" ]; then
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /home/webapp/.bashrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/webapp/.bashrc
+    echo 'export PATH="$PYENV_ROOT/shims:$PATH"' >> /home/webapp/.bashrc
+    echo 'eval "$(pyenv init -)"' >> /home/webapp/.bashrc
+fi
 
 source ~/.bashrc
-eval "$(pyenv init -)" 
+eval "$(pyenv init -)"  
 pyenv install 3.10.4 && pyenv global 3.10.4
 python -m pip install locust==2.16.1 locust-plugins==4.0.0 paho-mqtt==1.6.1 websocket-client==1.6.2 ujson
 _
