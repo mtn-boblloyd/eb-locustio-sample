e08db844af0c6623a0fa633ae9c168199b9f0cb3
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-03-04 16:20:59 -0500

    Update subnets to include more than one subnet.  Update build.sh to include additional parameters for launching locust.

--- build.sh
+++ build.sh
@@ -27,13 +27,13 @@ fi
 echo "Done with webapp user configuration, building the application file."
 
 if [ "$TYPE" == "server-controller" ]; then
-    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_server/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:10100 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --master -P 8088 --master-bind-port 5556" > application
+    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_server/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:11200 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --master -P 8088 --master-bind-port 5556 --operator-host https://$PRAGMA_HOST:11200 --login-schema https --http-port 11000 --operator-port 11200" > application
 elif [ "$TYPE" == "client-controller" ]; then
-    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_client/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:10100 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --master -P 8088 --master-bind-port 5556 --matchmaking-script "configs/websocket_matchmaking_full.json"" > application
+    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_client/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:11200 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --master -P 8088 --master-bind-port 5556 --matchmaking-script configs/websocket_matchmaking_full.json --operator-host https://$PRAGMA_HOST:11200 --login-schema https --http-port 11000 --operator-port 11200" > application
 elif [ "$TYPE" == "server-worker" ]; then
-    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_server/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:10100 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --worker --master-host spectre-pragma-load-test-controller-server.us-west-1.elasticbeanstalk.com --master-bind-port 5556" > application
+    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_server/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:11200 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --worker --master-host spectre-pragma-load-test-controller-server.us-west-1.elasticbeanstalk.com --master-bind-port 5556 --operator-host https://$PRAGMA_HOST:11200 --login-schema https --http-port 11000 --operator-port 11200" > application
 elif [ "$TYPE" == "server-worker" ]; then
-    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_client/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:10100 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --worker --master-host spectre-pragma-load-test-controller-client.us-west-1.elasticbeanstalk.com --master-bind-port 5556 --matchmaking-script configs/websocket_matchmaking_full.json" > application
+    echo "source ~/.bashrc && cd /var/app/current && python -m locust -f websocket_client/locustfile.py --tags load_test --host ws://$PRAGMA_HOST:11200 -u 200 -r 10 --login-schema http --http-port 11000 --operator-port 11200 --worker --master-host spectre-pragma-load-test-controller-client.us-west-1.elasticbeanstalk.com --master-bind-port 5556 --matchmaking-script configs/websocket_matchmaking_full.json --operator-host https://$PRAGMA_HOST:11200 --login-schema https --http-port 11000 --operator-port 11200" > application
 else
     echo "Something is wrong, and the type is not matched!"
     exit 1
