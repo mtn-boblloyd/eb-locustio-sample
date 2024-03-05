1417273cf9207085889e8b8e06e1b70990af1bf0
Bob Lloyd <bob.lloyd@mountaintop.gg>
2024-03-04 19:47:40 -0500

    Fix build.sh to have the correct controller DNS entries.  Fix build.sh to have client-worker specified.

--- startup_all.sh
+++ startup_all.sh
@@ -1,7 +1,12 @@
-eb init --keyname mtn_aws --platform "Corretto 21 running on 64bit Amazon Linux 2023" --region us-west-1 spectre-pragma-load-test
+PRAGMA_HOST=${1:-"dev.internal.spectre.mountaintop.pragmaengine.com"}
+SERVER_WORKER_COUNT=${2:-1}
+CLIENT_WORKER_COUNT=${3:-1}
+WORKER_NODE_TYPE=${4:-t2.micro}
 
-eb create --cname spectre-pragma-load-test-server-control --envvars TYPE=server-controller,PRAGMA_HOST=dev.internal.spectre.mountaintop.pragmaengine.com --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023" -im 1 -ix 1 spectre-pragma-load-test-controller-server
-eb create --cname spectre-pragma-load-test-server-worker --envvars TYPE=server-worker,PRAGMA_HOST=dev.internal.spectre.mountaintop.pragmaengine.com --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023" -im $SERVER_WORKER_COUNT -ix $SERVER_WORKER_COUNT spectre-pragma-load-test-worker-server 
+eb init --keyname mtn_aws --platform "Corretto 21 running on 64bit Amazon Linux 2023" --region us-west-1 spectre-lt
 
-eb create --cname spectre-pragma-load-test-client-control --envvars TYPE=client-controller,PRAGMA_HOST=dev.internal.spectre.mountaintop.pragmaengine.com --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023"  -im 1 -ix 1 us-west-1 spectre-pragma-load-test-controller-client
-eb create --cname spectre-pragma-load-test-client-worker --envvars TYPE=client-worker,PRAGMA_HOST=dev.internal.spectre.mountaintop.pragmaengine.com --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023" us-west-1 -im $CLIENT_WORKER_COUNT -ix $CLIENT_WORKER_COUNT spectre-pragma-load-test-worker-client
+eb create --cname spectre-lt-server-control --envvars TYPE=server-controller,PRAGMA_HOST=$PRAGMA_HOST --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023" -im 1 -ix 1 spectre-lt-controller-server
+eb create --cname spectre-lt-server-worker --envvars TYPE=server-worker,PRAGMA_HOST=$PRAGMA_HOST --instance_type $WORKER_NODE_TYPE --platform "Corretto 21 running on 64bit Amazon Linux 2023" -im $SERVER_WORKER_COUNT -ix $SERVER_WORKER_COUNT spectre-lt-worker-server 
+
+eb create --cname spectre-lt-client-control --envvars TYPE=client-controller,PRAGMA_HOST=$PRAGMA_HOST --instance_type t2.micro --platform "Corretto 21 running on 64bit Amazon Linux 2023"  -im 1 -ix 1 spectre-lt-controller-client
+eb create --cname spectre-lt-client-worker --envvars TYPE=client-worker,PRAGMA_HOST=$PRAGMA_HOST --instance_type $WORKER_NODE_TYPE --platform "Corretto 21 running on 64bit Amazon Linux 2023" -im $CLIENT_WORKER_COUNT -ix $CLIENT_WORKER_COUNT spectre-lt-worker-client
